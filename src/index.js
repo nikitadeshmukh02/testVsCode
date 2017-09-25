77e0da093cbcd98ce85a871368ae502a4cfcf071
Lalit Deshmukh <lalitadeshmukh@Lalits-MacBook-Air.local>
2017-09-25 08:46:32 -0400

    learning

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const App=function(){
+import React from 'react'
+import ReactDom from 'react-dom'
+const App=()=>{
 return <div>Hi!</div>
 }
+ReactDom.render(<App/>,document.querySelector('.container'))
